68d1d43897715a84973c3203812e6f33692a400c
RobinLaevaert <robin.laevaert@student.ap.be>
2018-05-06 21:48:46 +0200

    HomeComponent + navbar + 404 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,  
+    NavBarComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    FooterComponent
+    
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: "**", component: PageNotFoundComponent}
+    ], { useHash: true }),
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
