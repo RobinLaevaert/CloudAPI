e6229fa667d23e12f7d24bda229d162f84d2cd63
Unknown <robin.laevaert@student.ap.be>
2018-05-10 00:16:19 +0200

    Fixtures component

--- src/app/fixtures/fixtures.component.ts
+++ src/app/fixtures/fixtures.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SoccerService, competitionFixtures } from '../services/soccer.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fixtures',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fixtures.component.scss']
 })
 export class FixturesComponent implements OnInit {
+  fixtures : competitionFixtures.Fixture[];
+  id: number = 445;
+  matchday : number = 37;
+  constructor(private svc: SoccerService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  getColor(result:competitionFixtures.Result){
+    if(result.homeTeamWon == true){return "green"}
+    
   }
 
+  ngOnInit() {
+    this.svc.getCompetitionFixtures(this.id,this.matchday).subscribe(result => {
+      this.fixtures = result.fixtures;
+      this.fixtures.forEach(s => {
+        if(s.result.goalsHomeTeam == s.result.goalsAwayTeam){
+          s.result.draw = true;
+          s.result.homeTeamWon = false;
+          s.result.homeTeamColor = "orange";
+          s.result.awayTeamColor = "orange";
+        }
+        else if(s.result.goalsHomeTeam>s.result.goalsAwayTeam){
+          s.result.draw = false;
+          s.result.homeTeamWon = true;
+          s.result.homeTeamColor = "green";
+          s.result.awayTeamColor = "red";
+        }
+        else if(s.result.goalsHomeTeam<s.result.goalsAwayTeam){
+          s.result.draw = false;
+          s.result.homeTeamWon = false;
+          s.result.homeTeamColor = "green";
+          s.result.awayTeamColor = "red";
+        }
+        this.svc.getTeamFromURL(s._links.homeTeam.href).subscribe(result =>{
+          s.homeTeamCrest = result.crestUrl;
+        })
+        this.svc.getTeamFromURL(s._links.awayTeam.href).subscribe(result =>{
+          s.awayTeamCrest = result.crestUrl;
+        })
+      })
+    }) 
+  }
 }
